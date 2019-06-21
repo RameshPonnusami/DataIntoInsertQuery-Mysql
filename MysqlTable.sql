7879f24c3a9e095ff4f1bf7bcaea1b2749d6d5a2
RameshPonnusami <ramramesh1374@gmail.com>
2019-06-21 14:42:34 +0530

    Create MysqlTable.sql

---
+++ MysqlTable.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `thread_one` (  `EID` varchar(280) DEFAULT NULL, 
+`Name` varchar(280) DEFAULT NULL,     
+`Address` varchar(280) DEFAULT NULL, 
+`Age` varchar(280) DEFAULT NULL,
+`Gender` varchar(280) DEFAULT NULL,
+`Aadhar` varchar(280) DEFAULT NULL
+
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
