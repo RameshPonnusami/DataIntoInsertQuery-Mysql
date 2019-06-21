3205021bce62cd0bf5292c25a0e808bbefa740ad
RameshPonnusami <ramramesh1374@gmail.com>
2019-06-21 14:45:40 +0530

    Update MysqlTable.sql

--- MysqlTable.sql
+++ MysqlTable.sql
@@ -6,3 +6,15 @@ CREATE TABLE `thread_one` (  `EID` varchar(280) DEFAULT NULL,
 `Aadhar` varchar(280) DEFAULT NULL
 
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
+ Insert Into RDB.thread_two(EID ,Name ,Address ,Age,Gender,Aadhar ) VALUES
+ ("12","Adit ","Room 156, DG Hostel, IIT BHU","26","M","23452345345"),
+ ("14","Rahul ","Room 153, DG Hostel, IIT BHU","25","M","7878978"),
+ ("12","Adit ","Room 156, DG Hostel, IIT BHU","26","M","67878978"),
+ ("12","Adit ","Room 156, DG Hostel, IIT BHU","26","M","789789"),
+ ("15","ram","Room 124, Dhanraj Giri Hostel, IIT BHU","20","M","57657867"),
+ ("15","ram","Room 124, Dhanraj Giri Hostel, IIT BHU","20","M","786878"),
+ ("7","Mayank","Room 149, Dhanraj Giri Hostel, IIT BHU","22","M","6666"),
+ ("14","Rahul Prasad","Room 153, DG Hostel, IIT BHU","25","M","5555"),
+ ("1","Ramesh","Room 124, Dhanraj Giri Hostel, IIT BHU","20","M","4444"),
+ ("9","TAMIL","Room 171, DG, IIT BHU","25","M","4444"); 
